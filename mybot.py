853bb41997c87733192590f69f87af0d7c6195f9
Anna Bogacheva <annab070703@gmail.com>
2021-12-14 17:48:40 +0300

    dockerfile

--- mybot.py
+++ mybot.py
@@ -1,29 +1,38 @@
-#!/usr/bin/env python
-import random
-import re
+"""
+high level support for doing this and that.
+"""
+import random  # for giving random things
+import re  # for parsing messages
 
 import telebot
 
-bot = telebot.TeleBot("5097013736:AAHOj0LU2GXj8dE2DXDMZlg8vFL8QRdhy0Q", parse_mode=None)
+BOT = telebot.TeleBot("5097013736:AAHOj0LU2GXj8dE2DXDMZlg8vFL8QRdhy0Q", parse_mode=None)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@BOT.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    """
+    high level support for doing this and that.
+    """
+    BOT.reply_to(message, "Howdy, how are you doing?")
 
 
-@bot.message_handler(commands=['deadline'], content_types=['text'])
+@BOT.message_handler(commands=['deadline'], content_types=['text'])
 def send_random(message):
+    """
+    high level support for doing this and that.
+    """
     r_c = re.findall(r'\d+', message.text)
     if len(r_c) == 0:
-        bot.send_message(message.chat.id, "Ты совсем тупой? Из чего мне выбирать?")
+        BOT.send_message(message.chat.id, "Ты совсем тупой? Из чего мне выбирать?")
         return
     if len(r_c) == 1:
-        bot.send_message(message.chat.id, "Пидор, 2 цифры надо")
+        BOT.send_message(message.chat.id, "Пидор, 2 цифры надо")
         return
     r_min = int(r_c[0])
     r_max = int(r_c[1])
-    bot.send_message(message.chat.id, 'Сейчас стоит делать: {}'.format(random.randint(r_min, r_max)))
+    BOT.send_message(message.chat.id,
+                     'Сейчас стоит делать: {}'.format(random.randint(r_min, r_max)))
 
 
-bot.infinity_polling()
+BOT.infinity_polling()
