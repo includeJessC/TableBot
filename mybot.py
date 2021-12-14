14eb8cf2fe4c912e7bd41af9358ece1fc4e9d2ea
Anna Bogacheva <annab070703@gmail.com>
2021-12-15 00:17:41 +0300

    fix jobs

--- mybot.py
+++ mybot.py
@@ -1,13 +1,11 @@
 """
 high level support for doing this and that.
 """
-import random  # for giving random things
-import re  # for parsing messages
 import os
 
 import telebot
 
-from prettytable import PrettyTable
+from prettytable import PrettyTable, from_html_one
 
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 BOT = telebot.TeleBot(TOKEN, parse_mode=None)
@@ -21,49 +19,91 @@ def send_welcome(message):
     BOT.reply_to(message, "Howdy, how are you doing?")
 
 
-@BOT.message_handler(commands=['deadline'], content_types=['text'])
-def send_random(message):
-    """
-    random number.
-    """
-    r_c = re.findall(r'\d+', message.text)
-    if len(r_c) == 0:
-        BOT.send_message(message.chat.id, "Ты совсем тупой? Из чего мне выбирать?")
-        return
-    if len(r_c) == 1:
-        BOT.send_message(message.chat.id, "Пидор, 2 цифры надо")
-        return
-    r_min = int(r_c[0])
-    r_max = int(r_c[1])
-    BOT.send_message(message.chat.id,
-                     f'Сейчас стоит делать: {random.randint(r_min, r_max)}')
-
-
 @BOT.message_handler(commands=['table_text'], content_types=['text'])
 def send_table(message):
     """
     making table
     """
     table = PrettyTable()
-    list_words = re.findall(r'\d+', message.text)
-    if len(list_words) < 2:
+    list_words = message.text.split()
+    if len(list_words) < 3:
         BOT.send_message(message.chat.id, 'Не надо так')
         return
-    number_of_fields = int(list_words[0])
+    number_of_fields = int(list_words[1])
     if number_of_fields == 0:
         BOT.send_message(message.chat.id, 'Не надо так')
         return
     row = []
-    for i in range(2, len(list_words)):
-        if i < len(list_words) + number_of_fields:
-            table.field_names.append(list_words[i])
+    fields = []
+    for i in range(3, len(list_words)):
+        if i < 3 + number_of_fields:
+            fields.append(str(list_words[i]))
         else:
+            if i == 3 + number_of_fields:
+                table.field_names = fields
             if len(row) == number_of_fields:
                 table.add_row(row)
-                row = []
+                elem = str(list_words[i])
+                if list_words[i].isdigit():
+                    elem = int(list_words[i])
+                row = [elem]
             else:
-                row.append(list_words[i])
+                elem = str(list_words[i])
+                if list_words[i].isdigit():
+                    elem = int(list_words[i])
+                row.append(elem)
+    table.add_row(row)
     string_s = table.get_string()
+    BOT.send_message(message.chat.id,
+                     string_s)
+
+
+@BOT.message_handler(commands=['table_html'], content_types=['text'])
+def send_table(message):
+    """
+    making table
+    """
+    table = PrettyTable()
+    list_words = message.text.split()
+    if len(list_words) < 3:
+        BOT.send_message(message.chat.id, 'Не надо так')
+        return
+    number_of_fields = int(list_words[1])
+    if number_of_fields == 0:
+        BOT.send_message(message.chat.id, 'Не надо так')
+        return
+    row = []
+    fields = []
+    for i in range(3, len(list_words)):
+        if i < 3 + number_of_fields:
+            fields.append(str(list_words[i]))
+        else:
+            if i == 3 + number_of_fields:
+                table.field_names = fields
+            if len(row) == number_of_fields:
+                table.add_row(row)
+                elem = str(list_words[i])
+                if list_words[i].isdigit():
+                    elem = int(list_words[i])
+                row = [elem]
+            else:
+                elem = str(list_words[i])
+                if list_words[i].isdigit():
+                    elem = int(list_words[i])
+                row.append(elem)
+    table.add_row(row)
+    string_s = table.get_html_string()
+    BOT.send_message(message.chat.id,
+                     string_s)
+
+
+@BOT.message_handler(commands=['to_text'], content_types=['text'])
+def send_table(message):
+    """
+    making table
+    """
+    list_words = message.text[8:]
+    string_s = from_html_one(list_words).get_string()
     BOT.send_message(message.chat.id,
                      string_s)
 
